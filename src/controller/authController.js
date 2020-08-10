13586974205ddbb97a92d5ced9177cd3a3926861
viniciusleal34 <56416018+viniciusleal34@users.noreply.github.com>
2020-08-10 11:11:41 -0300

    [Add] Rota de atualização arrumada

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require('express')
 const bcrypt = require('bcryptjs')
-const User = require('../model/User');
+const User = require('../model/User')
 const jwt = require('jsonwebtoken')
 
 const router = express.Router();
@@ -17,60 +17,43 @@ function geradorToken(parms={}){
 
 router.post('/register', async(req, res)=>{
     const { email, cpf} = req.body
-  
+    
     try{
-        if(await User.findOne({ email })){
+        if(await User.findOne({ email }))
             return res.status(400).send({ error: 'Email existente!'})
-        }
-        if(await User.findOne({ cpf })){
-            return res.status(400).send({ error: 'CPF existente!'})
-        }
         
+        if(await User.findOne({ cpf }))
+            return res.status(400).send({ error: 'CPF existente!'})     
         const user = await User.create(req.body);
         user.password = undefined;
-
         return res.send({ user,
             token: geradorToken({id: user.id}) });
     }catch(err){
         return res.status(400).send({ error: 'Registration failed'})
     }
-
 })
 
 router.post('/authenticated', async(req, res)=>{
-
     const{email, password} = req.body;
-
-    
     const user = await User.findOne({email}).select('+password')
     let usuario = user
     const cpf_login = await User.findOne({'cpf':email}).select('+password')
 
     if(!user){
-        if(!cpf_login){
-            return res.status(400).send({error:'Usuário não encontrado'})
-        }
-        usuario = cpf_login
-       
+        if(!cpf_login)
+            return res.status(400).send({error:'Usuário não encontrado'})       
+        usuario = cpf_login   
     }
-
-    if(usuario.nivel == 0){
-        
+    if(usuario.nivel == 0)
         return res.status(400).send({error:'Usuário Desativado '})
-    }
-
-    if( !await bcrypt.compare(password, usuario.password) ){
+    if( !await bcrypt.compare(password, usuario.password) )
         return res.status(400).send({error:'Senha Invalida'})
-    }
-
     usuario.password = undefined
-
     res.send({usuario,     
         token: geradorToken({id: usuario.id})
     })
-
 })
 
 
 
-module.exports = app => app.use('/auth', router);
+module.exports = app => app.use('/auth', router)
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,42 +1,53 @@
-const express = require('express');
+const express = require('express')
 const authMiddlewares = require('../middlewares/auth')
-const User = require('../model/User');
-const router = express.Router();
+const User = require('../model/User')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 router.use(authMiddlewares)
 
 router.get('/todos', async(req, res) => {
-   adm = await User.find({'_id': req.userId})
-   if(adm[0].nivel === 9999)
-   {
-      user = await User.find({})
+   const adm = await User.find({'_id': req.userId})
+
+   if(adm[0].nivel === 9999){
+      const user = await User.find({})
       return res.send( {user})
-   }
+      }
    return res.status(400).send({ error: 'Usuario não permetido'})
 })
 
+
+
 router.get('/ativos/:id', async(req, res) => {
- try { 
+ try {
    const user = await User.findById(req.params.id)
    res.send( {user})
 }
 catch(err){
-
+   return res.status(400).send({ error: 'Erro'})
 }
 })
+
+
 
 router.put('/:id', async(req, res)=>{
- try { 
    const update = req.body
+try {
 
-   await User.updateOne({'_id':req.params.id}, update);
-   const userat = await User.findById(req.params.id)
-   res.send( {userat})
-}
+   if (update.password){
+      const hash = await bcrypt.hash(update.password, 10)
+      update.password = hash
+   }
+      await User.update({'_id':req.params.id}, update)
+
+      const user = await User.findById(req.params.id)
+      res.send( {user})
+   }
    catch(err){
       return res.send({user:'erro'})}
-   
+
 })
 
 
+// eslint-disable-next-line no-undef
 module.exports = app => app.use('/users', router)
